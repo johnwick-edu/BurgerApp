f5ef31ee098de98756a73de3ec2ea2c78612e8a4
Aysham <aysham254@gmail.com>
2024-11-27 14:25:20 +0200

    Adding the burger class

---
+++ src/main/java/Models/Burger.java
@@ -0,0 +1,40 @@
+ 
+package Models;
+
+import java.util.ArrayList;
+
+ 
+public class Burger {
+    
+    private ArrayList<String> toppings;   // List to store burger toppings
+    
+    private static final int MAX_TOPPINGS = 5; // Max number of toppings
+    
+    private static final double BURGER_PRICE = 20.0; // Base price of a burger
+    
+    private static final double TOPPING_PRICE = 2.0; // Price of each toppping
+    
+    public Burger(){
+        // Initializing the toppings list
+        toppings = new ArrayList<>();
+    }
+    
+    public void addTopping(String topping) throws Exception{
+        if(toppings.size()<MAX_TOPPINGS){
+            toppings.add(topping);
+        }else{
+            throw new Exception("Maximum number of toppings reached!");
+        }
+    }
+
+    public double calculatePrice(){
+        return BURGER_PRICE + (TOPPING_PRICE * toppings.size());
+    }
+    
+    @Override
+    public String toString() {
+        return "Burger with toppings: "+toppings +"\n Total Price: R "+calculatePrice();
+    }
+    
+    
+}
